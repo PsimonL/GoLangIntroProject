21a238531c845942c2e475d9800b42a6ec657faf
Szymon Rogowski <szymon0r0official@gmail.com>
2022-11-01 02:04:44 +0100

    aaaaaaaaa

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"net/http"
+)
 
+// https://pkg.go.dev/net/http#FileServer
+// https://go.dev/doc/articles/wiki/
+// https://www.digitalocean.com/community/tutorials/how-to-make-an-http-server-in-go
+// https://www.youtube.com/watch?v=jFfo23yIWac&t=611s
+
+func main() {
+	fileServer := http.FileServer()
 }
