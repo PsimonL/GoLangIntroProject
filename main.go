c9aa81a6bddd3c444ed5d9e36b9e73598c930d29
Szymon Rogowski <szymon0r0official@gmail.com>
2022-11-01 03:44:18 +0100

    4

--- main.go
+++ main.go
@@ -12,27 +12,35 @@ import (
 // https://www.youtube.com/watch?v=jFfo23yIWac&t=611s
 
 func formHandler(w http.ResponseWriter, r *http.Request) { // response - request
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	surname := r.FormValue("surname")
+	fmt.Fprintf(w, "name = %s\n", name)
+	fmt.Fprintf(w, "surname = %s\n", surname)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
-		http.Error(w, "404 Not Found", http.StatusNotFound)
+		http.Error(w, "404 not found", http.StatusNotFound)
 		return
 	}
 	if r.Method != "GET" { // by default used GET
-		http.Error(w, "Method from CRUD doesnt supported", http.StatusNotFound)
+		http.Error(w, "method is not supported", http.StatusNotFound)
 		return
 	}
 	fmt.Fprintf(w, "hello!")
-}
-
-func helloHandler() {
-
 }
 
 func main() {
 	fileServer := http.FileServer(http.Dir("./static")) // default points to index.html
 	http.Handle("/", fileServer)                        // root route
 	http.HandleFunc("/form", formHandler)
-	http.HandleFunc("/form", helloHandler)
-
+	http.HandleFunc("/hello", helloHandler)
+	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err) // https://www.reddit.com/r/golang/comments/dbw0uc/need_clarification_on_the_use_of_semicolon_after/
 	}
